c26d4dbd50d34168c079feb41775f41b104ba099
kristiyanpetkov <kristiqn.l.petkov@gmail.com>
2015-10-15 10:50:55 +0300

    added more test in BoundaryAgeValidatorTest class

--- src/com/clouway/service/Person.java
+++ src/com/clouway/service/Person.java
@@ -3,9 +3,9 @@ package com.clouway.service;
 /**
  * Created by clouway on 15-9-30.
  */
-public class Person {
-    public String name;
-    public String age;
+public final class Person {
+    public final String name;
+    public final String age;
 
     public Person(String name, String age) {
         this.name = name;
--- test/com/clouway/service/BoundaryAgeValidatorTest.java
+++ test/com/clouway/service/BoundaryAgeValidatorTest.java
@@ -11,21 +11,35 @@ public class BoundaryAgeValidatorTest {
     @Test
     public void happyPath(){
         final Person person = new Person("Unknown","20");
-       final BoundaryAgeValidator bulgarianAgeValidator = new BoundaryAgeValidator(10,80);
+       final BoundaryAgeValidator bulgarianAgeValidator = new BoundaryAgeValidator(18,80);
         assertEquals(true,bulgarianAgeValidator.isAdult(person.age));
     }
 
     @Test
-    public void lowerThanLowerBoundary(){
+    public void ageLowerThanLowerBoundary(){
         final Person person = new Person("Unknown","6");
-        final BoundaryAgeValidator bulgarianAgeValidator = new BoundaryAgeValidator(10,80);
+        final BoundaryAgeValidator bulgarianAgeValidator = new BoundaryAgeValidator(18,80);
         assertEquals(false,bulgarianAgeValidator.isAdult(person.age));
     }
 
     @Test
-    public void higherThanHigherBoundary(){
+    public void ageHigherThanHigherBoundary(){
         final Person person = new Person("Unknown","90");
-        final BoundaryAgeValidator bulgarianAgeValidator = new BoundaryAgeValidator(10,80);
+        final BoundaryAgeValidator bulgarianAgeValidator = new BoundaryAgeValidator(18,80);
         assertEquals(false,bulgarianAgeValidator.isAdult(person.age));
+    }
+
+    @Test
+    public void ageEqualToLowerBoundary(){
+        final Person person = new Person("Unknown","18");
+        final BoundaryAgeValidator bulgarianAgeValidator = new BoundaryAgeValidator(18,80);
+        assertEquals(true,bulgarianAgeValidator.isAdult(person.age));
+    }
+
+    @Test
+    public void ageEqualToHigherBoundary(){
+        final Person person = new Person("Unknown","80");
+        final BoundaryAgeValidator bulgarianAgeValidator = new BoundaryAgeValidator(18,80);
+        assertEquals(true,bulgarianAgeValidator.isAdult(person.age));
     }
 }
